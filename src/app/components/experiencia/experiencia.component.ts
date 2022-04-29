30a54184b2e4216da6b27c7c4c9c4a92dd93d34e
Natalia Maggi <86575010+NatMaggi@users.noreply.github.com>
2022-04-28 21:09:18 -0300

    arranque del back

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicio/portfolio.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
+  educacionList:any;
+  experienciaList:any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
-  }
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.educacionList=data.educacion;
+      this.experienciaList=data.experiencia;
+  })
 
+}
 }
